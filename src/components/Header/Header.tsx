a52f1354176d7549d6972bf088fe89cc55a928e9
alvaro bianor <bianor.alvaro12@gmail.com>
2023-04-01 14:54:19 -0300

    Finishing the global css and header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css'
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>IgFeed</strong>
+      <img src="/src/assets/ignite-logo.svg" alt="logo" />
     </header>
   )
 }
